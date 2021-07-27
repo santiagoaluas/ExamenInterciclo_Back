d7d614f8feac745f995cac33c154734a587fd7e6
Santiago Alulema <salulema@sistec.com.ec>
2021-07-27 10:07:37 -0500

    agregar mas cors

--- Startup.cs
+++ Startup.cs
@@ -41,7 +41,7 @@ namespace ExamenInterciclo_Back
             {
                 opciones.AddDefaultPolicy(builder =>
                 {
-                    builder.WithOrigins("http://localhost:4202", "https://santiagoaluas.github.io")
+                    builder.WithOrigins("http://localhost:4202", "https://santiagoaluas.github.io", "http://localhost", "http://localhost", "http://localhost:8080")
                     .AllowAnyMethod()
                     .AllowAnyHeader();
                 });
